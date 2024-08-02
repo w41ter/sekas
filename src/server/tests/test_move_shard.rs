1da560f7c3ac9faf144ff89d3a6d491a1fb95fdc
w41ter <w41ter.l@gmail.com>
2024-08-02 19:14:57 +0800

    Rename integration tests

--- src/server/tests/test_move_shard.rs
+++ src/server/tests/test_move_shard.rs
@@ -1,4 +1,4 @@
-// Copyright 2023 The Sekas Authors.
+// Copyright 2023-present The Sekas Authors.
 // Copyright 2022 The Engula Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
